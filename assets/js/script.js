3d259b8553ec58b8adab54d802fb3af64c6f8b5f
Fransiscus Kevin <81698880+kvnsurya666@users.noreply.github.com>
2023-12-04 16:42:25 +0700

    revision 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,22 @@
+function getValue() {
+  var select = document.getElementById("selectOption");
+  var luas = select.options[select.selectedIndex].value;
+  document.getElementById("luas").innerHTML = luas;
+  var keliling = select.options[select.selectedIndex].value;
+  document.getElementById("keliling").innerHTML = keliling;
+}
+
 function hitung() {
   var sisi1 = document.getElementById("sisi1").value;
   var sisi2 = document.getElementById("sisi2").value;
-  var tinggi = document.getElementById("tanggal").value;
+  var sisi3 = document.getElementById("sisi3").value;
+  var tinggi = document.getElementById("tinggi").value;
 
-  if (sisi1 == "" || sisi2 == "" || tinggi == "") {
+  if (sisi1 == "" || sisi2 == "" || sisi3 == "" || tinggi == "") {
     alert("Mohon isi semua input");
   } else {
     var luas = 0.5 * sisi1 * tinggi;
-    var keliling = sisi1 + sisi2 + tinggi;
+    var keliling = sisi1 + sisi2 + sisi3;
 
     document.getElementById("luas").innerHTML = "Luas Segitiga: " + luas;
     document.getElementById("keliling").innerHTML =
